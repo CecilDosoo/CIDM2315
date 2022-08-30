2d9a9dc3fc68bffe559e19162a025a4637eb86ca
CecilDosoo <107536376+CecilDosoo@users.noreply.github.com>
2022-08-30 05:28:05 +0300

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5;
+    double y = 3.3;  
+    double z = (4*x*x) + 3*y;
+    
+    Console.WriteLine ($"The value of Z is: {z}");
+    }
+}

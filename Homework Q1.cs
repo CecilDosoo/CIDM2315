bd680298d018a3fb73a50a3ae8cbc811128d0cb6
CecilDosoo <107536376+CecilDosoo@users.noreply.github.com>
2022-09-07 23:52:32 -0500

    Add files via upload

---
+++ Homework Q1.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework2;
+class Program
+{
+    static void Main(string[] args)
+    {   
+
+        Console.WriteLine("Please input a letter grade:");
+    string letter = Console.ReadLine();
+    switch (letter) 
+        {    case "A":
+            Console.WriteLine("GPA point: 4");
+            break;
+            case "B":
+            Console.WriteLine("GPA point: 3");
+             break;
+            case "C":
+            Console.WriteLine("GPA point: 2");
+            break;
+             case "D":
+            Console.WriteLine("GPA point: 1");
+             break;
+              case "F":
+            Console.WriteLine("GPA point: 0");
+             break;
+            default:
+            Console.WriteLine("Wrong Letter Grade!");
+            break;
+    }
+}
+}
---
+++ Homework Q2.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework3;
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.WriteLine("Please input the first num: ");
+        int n1 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Please input the second num: ");
+        int n2 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Please input the third num: ");
+        int n3 = Convert.ToInt16(Console.ReadLine());
+
+        if (n1 < n2){
+            if (n1 < n3){
+                Console.WriteLine($"The smallest number is {n1}");
+                }
+            else{
+                Console.WriteLine($"The smallest number is {n3}");
+                }}
+        else{
+            if (n2 < n3){
+                Console.WriteLine($"The smallest number is {n2}");
+            }
+        else{
+            Console.WriteLine($"The smallest number is {n3}");
+        }
+        }
+}
+}
+

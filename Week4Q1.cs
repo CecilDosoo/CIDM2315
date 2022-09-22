6bf30dfaa4b8bb8040d859cd849baf664d3a890e
CecilDosoo <107536376+CecilDosoo@users.noreply.github.com>
2022-09-21 23:41:34 -0500

    Add files via upload

---
+++ Week4Q1.cs
@@ -0,0 +1,24 @@
+﻿namespace week4homework;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter two integers: ");
+        int a = Convert.ToInt16(Console.ReadLine());
+        int b = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine($"a = {a}; b = {b}");
+
+        if (a > b)
+        {
+            Console.WriteLine($"The Largest number is: {a}");
+        }
+        else
+        {
+            Console.WriteLine($"The Largest number is: {b}");
+        }
+
+    }
+
+
+}

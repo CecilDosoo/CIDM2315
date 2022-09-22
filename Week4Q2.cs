fd5685c6eabd36cd080d830607f601898bb82b42
CecilDosoo <107536376+CecilDosoo@users.noreply.github.com>
2022-09-21 23:45:07 -0500

    Add files via upload

---
+++ Week4Q2.cs
@@ -0,0 +1,46 @@
+﻿namespace week4Q2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter an integer: ");
+        int n = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("Enter a shape: ");
+        string shape = Console.ReadLine();
+
+        Console.WriteLine($"N is: {n}; shape is {shape}");
+
+
+        if(shape == "left")
+        {
+            for(int row = 0; row<n; row++)
+            {
+                for(int col = 0; col<n; col++)
+                {
+                if(row>=col)
+                Console.Write('*');
+                }
+                Console.WriteLine("");
+            }
+        }
+        if(shape == "right")
+        {
+    
+        int a, b, c;
+        for (a = 1; a <= n; a++)
+        {
+            for (b = 1; b <= n - a; b++)
+            {
+                Console.Write(" ");
+            }
+            for (c = 1; c <= a; c++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+        } 
+        }
+    }
+}
+

988cd1e5618a871a1732f7934bf1db27d4bb72c7
CecilDosoo <107536376+CecilDosoo@users.noreply.github.com>
2022-10-19 23:17:59 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,48 @@
-﻿namespace week4homework2;
+﻿namespace Homework7;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter an integer: ");
-        int n = Convert.ToInt16(Console.ReadLine());
+        Customer cust1 = new Customer(cus_id:110, cus_name: "Alice", cus_age: 28);
+        Customer cust2 = new Customer(cus_id:111, cus_name: "Bob", cus_age: 30);
 
-        Console.WriteLine("Enter a shape: ");
-        string shape = Console.ReadLine();
+        cust1.PrintCusInfo();
+        cust2.PrintCusInfo();
 
-        Console.WriteLine($"N is: {n}; shape is {shape}");
+        cust1.ChangeID(220);
+        cust2.ChangeID(221);
+
+        cust1.PrintCusInfo();
+        cust2.PrintCusInfo();
+
+        cust1.CompareAge(cust2);
 
 
-        if(shape == "left")
-        {
-            for(int row = 0; row<n; row++)
-            {
-                for(int col = 0; col<n; col++)
-                {
-                if(row>=col)
-                Console.Write('*');
-                }
-                Console.WriteLine("");
-            }
-        }
-        if(shape == "right")
-        {
-    
-        int a, b, c;
-        for (a = 1; a <= n; a++)
-        {
-            for (b = 1; b <= n - a; b++)
-            {
-                Console.Write(" ");
-            }
-            for (c = 1; c <= a; c++)
-            {
-                Console.Write("*");
-            }
-            Console.WriteLine("");
-        } 
-        }
     }
 }
 
-
+class Customer {
+    private int cus_id;
+    public string cus_name;
+    public int cus_age;
+    public Customer(int cus_id, string cus_name, int cus_age){
+        this.cus_id = cus_id;
+        this.cus_name = cus_name;
+        this.cus_age = cus_age;
+    }
+    public void ChangeID(int new_id){
+        cus_id = new_id;
+    }
+    public void PrintCusInfo(){
+        Console.WriteLine($"Customer: {cus_id}, name: {cus_name}, age: {cus_age}");
+    }
+    public void CompareAge(Customer objCustomer){
+        if(cus_age > objCustomer.cus_age){
+            Console.WriteLine($"{cus_name} is older");
+        }
+        if(objCustomer.cus_age > cus_age){
+            Console.WriteLine($"{objCustomer.cus_name} is older");
+        }
+        
+    }
+}
